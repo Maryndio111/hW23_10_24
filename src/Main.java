6c85e2110c81aaefa849c9fe3b9c90b52745c19b
Marina Darievych <marindio111@gmail.com>
2024-10-22 22:28:33 -1200

    commit zwei

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        List<Integer>numbers = List.of(10,44,7,6,12,89);
+        int theshold = 33;
+        List<Integer>result = filterNumberGreaterThen(numbers,theshold);
+        System.out.println(result);//вывод 44,89
 
     }
+
+    private static List<Integer> filterNumberGreaterThen(
+            List<Integer> numbers, int theshold) {
+        List<Integer>filteredList = new ArrayList<>();/*новый список для храниения больших чисел,//
+        */
+        for (Integer number : numbers){
+            if (number>theshold){
+                filteredList.add(number);
+
+            }
+        }
+        return filteredList;
+    }
+
+
+
+
 }
